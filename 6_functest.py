7351ce2edb3d959638b4b81dafe34de77c1e087d
Minah <rinette9612@gmail.com>
2022-03-21 16:15:45 +0900

    함수

--- 6_functest.py
+++ 6_functest.py
@@ -36,3 +36,23 @@ for self_num in sorted(self_numbers):
 # set = 집합함수 / .add(num) 집합에 요소를 추가할 때
 # 리스트 정렬 .sort() 메서드 또는 .sorted()
 # str(num) 숫자 형태로는 각 자릿수를 분리할 수 없기 때문
+
+
+# 한수 (1065번)
+
+# 어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다.
+# 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다.
+# N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
+
+def hansu(num) :
+    hansu_cnt = 0
+    for i in range(1, num+1):
+        num_list = list(map(int, str(i))) # 문자열 분리
+        if i < 100:
+            hansu_cnt += 1  # 100보다 작으면 모두 한수
+        elif num_list[0]-num_list[1] == num_list[1]-num_list[2]:
+            hansu_cnt += 1  # x의 각 자리가 등차수열이면 한수
+    return hansu_cnt
+
+num = int(input())
+print(hansu(num))
