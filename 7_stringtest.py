8bbf83ee97cb3ffc7f5077c90782b1a8bd7fd374
Minah <rinette9612@gmail.com>
2022-03-23 15:00:35 +0900

    문자열 ~ 5622번

--- 7_stringtest.py
+++ 7_stringtest.py
@@ -77,3 +77,67 @@ for n in nums:
     new_n = c[0] + (10 * c[1]) + (100 * c[2])
     new_nums.append(new_n)
 print(max(new_nums))
+
+
+# 다이얼 (5622번)
+
+# 전화를 걸고 싶은 번호가 있다면, 숫자를 하나를 누른 다음에 금속 핀이 있는 곳 까지 시계방향으로 돌려야 한다. 숫자를 하나 누르면 다이얼이 처음 위치로 돌아가고, 다음 숫자를 누르려면 다이얼을 처음 위치에서 다시 돌려야 한다.
+# 숫자 1을 걸려면 총 2초가 필요하다. 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
+# 상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다. 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다. 예를 들어, UNUCIC는 868242와 같다.
+# 할머니가 외운 단어가 주어졌을 때, 이 전화를 걸기 위해서 필요한 최소 시간을 구하는 프로그램을 작성하시오.
+
+from string import ascii_uppercase
+
+upper = list(ascii_uppercase)
+abc = upper[0:3]
+def_ = upper[3:6]
+ghi = upper[6:9]
+jkl = upper[9:12]
+mno = upper[12:15]
+pqrs = upper[15:19]
+tuv = upper[19:22]
+wxyz = upper[22:26]
+
+word = input()
+word_list = list(word)
+
+result = []
+for i in word_list:
+    if i in abc:
+        i = 3
+        result.append(i)
+    elif i in def_:
+        i = 4
+        result.append(i)
+    elif i in ghi:
+        i = 5
+        result.append(i)
+    elif i in jkl:
+        i = 6
+        result.append(i)
+    elif i in mno:
+        i = 7
+        result.append(i)
+    elif i in pqrs:
+        i = 8
+        result.append(i)
+    elif i in tuv:
+        i = 9
+        result.append(i)
+    else:
+        i = 10
+        result.append(i)
+print(sum(result))
+
+# 해답
+
+dial_text = ['1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+gm = input()
+t = 0
+
+for i in gm:
+    for j in dial_text:
+        for k in j:
+            if k == i:
+                t = t + 2 + dial_text.index(j)
+print(t)
